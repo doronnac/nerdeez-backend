dfee396e4710c7b1e69e7e4c312c7176e5928e67
doronnac <doron.nachshon@outlook.com>
2013-05-15 17:16:19 +0300

    added put and delete tests to tests.py

--- nerdeez_backend_app/tests.py
+++ nerdeez_backend_app/tests.py
@@ -6,9 +6,20 @@ tests file for models.py
 @copyright: nerdeez.com
 '''
 
+#------------------------------------------------------------------------------ begin imports
+
 from tastypie.test import ResourceTestCase
 from nerdeez_backend_app.models import University
 
+#------------------------------------------------------------------------------ end imports
+
+
+#------------------------------------------------------------------------------ begin constants
+
+university_url = '/api/v1/university/'
+current_format = 'json'
+
+#------------------------------------------------------------------------------ end constants
 class UniversityResourceTest(ResourceTestCase):
     fixtures=['universities']
     def setup(self):
@@ -16,13 +27,19 @@ class UniversityResourceTest(ResourceTestCase):
     
     def test_university_creation(self):
         
-        #check that get and post commands work
+        #test that get works
         print University.objects.all()
-        resp = self.api_client.get('/api/v1/university/', format='json')
+        resp = self.api_client.get(university_url, format=current_format)
         self.assertHttpOK(resp)
         obj = self.deserialize(resp)
         self.assertEqual(obj['objects'][0]['title'], 'Namek', 'Failed to assertTitle')
         
-        resp = self.api_client.post('/api/v1/university/', format='json', data={'title':'technion','description':'A really good place to study','image':'technionimg','website':'www.technion.ac.il'})
+        resp = self.api_client.post(university_url, format=current_format, data={'title':'technion','description':"A really good place to study",'image':'technionimg','website':'www.technion.ac.il'})
         self.assertHttpCreated(resp)
         
+        #test that the user can't update any of the universities data
+        resp = self.api_client.put(university_url + '1/', current_format, data={'title': "UGOTHACKED"})
+        self.assertHttpMethodNotAllowed(resp)
+        
+        resp = self.api_client.delete(university_url + '1/', current_format)
+        self.assertHttpMethodNotAllowed(resp)
